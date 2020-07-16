89527d7dd4e4fcf4b5574b63b3a85195af6814b2
Preston Niayesh <pniayesh@berkeley.edu>
2020-07-15 21:55:33 -0700

    Backup finished.

--- src/Backup.java
+++ src/Backup.java
@@ -1,14 +1,51 @@
-import java.io.File;
-import java.io.IOException;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.FileFilterUtils;
+import org.apache.commons.io.filefilter.IOFileFilter;
+
+import java.io.*;
+import java.text.SimpleDateFormat;
 
 public class Backup {
 
-    public static void listFiles() throws IOException {
-        File mc = new File("/Users/prestonniayesh/Desktop/minecraft");
-        for (String s: mc.list()) {
-            System.out.println(s);
+    private String path;
+
+    private String msg;
+
+    public Backup(String message) {
+        path = "/Users/prestonniayesh/Desktop/minecraft/backup";
+        msg = message;
+    }
+
+    public void prepare() throws IOException {
+        File backup = new File(path);
+        if (backup.exists()) {
+            FileUtils.cleanDirectory(backup);
+        } else {
+            backup.mkdir();
         }
-        System.out.println("ran");
+    }
+
+    public void addInfo() throws IOException {
+        File info = new File(path + "/info.txt");
+        info.createNewFile();
+        FileWriter fW = new FileWriter(path + "/info.txt");
+        PrintWriter pW = new PrintWriter(fW);
+        String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new java.util.Date());
+        msg = timeStamp + "\n" + msg;
+        pW.printf("%s" + "%n", msg);
+        pW.close();
+    }
+
+    public void create() throws IOException {
+        prepare();
+        addInfo();
+        File src = new File("/Users/prestonniayesh/Desktop/minecraft");
+        File dest = new File(path);
+        IOFileFilter txtSuffixFilter = FileFilterUtils.suffixFileFilter(".jar");
+        FileFilter filter = FileFilterUtils.notFileFilter(txtSuffixFilter);
+        FileUtils.copyDirectory(src, dest, filter);
+        File extra = new File(path + "/backup");
+        FileUtils.deleteDirectory(extra);
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,14 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Backup.listFiles();
+        if (args[0].equals("backup")) {
+            if (args.length == 1 || args[1].isEmpty()) {
+                System.out.println("Please enter a backup message.");
+                System.exit(0);
+            } else {
+                Backup backup = new Backup(args[1]);
+                backup.create();
+            }
+        }
     }
 }
